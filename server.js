2e116ce40761ae6b6e954578e7486a826e446f5a
POOJITHA <127126687+MuddamPoojithaa@users.noreply.github.com>
2025-11-25 14:24:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://friendly-mooncake-655948.netlify.app"
+  ],
+  methods: "GET,POST,PUT,DELETE",
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // serve images
 
